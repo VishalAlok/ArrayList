6f6777667e1a7a268733665b17c0cc8ce0ab00e0
vishal.Alok <you@example.com>
2021-10-14 09:25:27 +0530

    Add project files.

---
+++ ArrayList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Array_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //Arraylist
+            ArrayList obj = new ArrayList();
+            Console.WriteLine(obj.Capacity); // 0
+            obj.Add(100);
+            Console.WriteLine(obj[0]); // 100
+            Console.WriteLine(obj.Capacity); // 0
+            obj.Add(200);
+            obj.Add(300);
+            obj.Insert(1,150);
+            foreach(object O in obj)
+            {
+                Console.Write(O + " "); //100 150 200 300
+            }
+            obj.RemoveAt(2);
+            foreach(object O in obj)
+            {
+                Console.Write(O + " ");//100 150 300
+            }
+        }
+    }
+}
